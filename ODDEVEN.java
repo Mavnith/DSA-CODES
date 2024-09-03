8030523a0d1748384767986d904e5448d6c8e05e
Mavnith <mavnith23@gmail.com>
2024-09-03 13:56:15 +0530

    Create ODDEVEN.java
    This code prints whether the number is ODD or EVEN

---
+++ ODDEVEN.java
@@ -0,0 +1,15 @@
+package JavaPlacement;
+
+public class EvenOdd {
+public static void main(String[]args) {
+	int n=9;
+	
+	if(n%2==0)
+	{
+		System.out.println("Given number is Even");
+	}
+	else {
+		System.out.println("Given number is not a Even");
+	}
+}
+}
